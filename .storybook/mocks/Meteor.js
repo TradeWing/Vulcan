92309f84409f01066b05efab11ed110e968f4240
Doron Roberts-Kedes <doronrk@gmail.com>
2020-06-20 19:25:46 -0700

    more mocks

--- .storybook/mocks/Meteor.js
+++ .storybook/mocks/Meteor.js
@@ -1,10 +1,21 @@
+const MeteorSettings = {
+  public: {
+    tenant: 'mock_tenant',
+    featureFlags: {
+      jobs: true,
+      events: true,
+      reporting: true,
+    },
+  },
+};
+
 // FIXME: we can't use ES6 imports in mocks, not sure why
 module.exports = {
-    settings: { public: { tenant: 'mock_tenant'} },
-    userId: () => 'mock_user_id',
-    startup: () => { },
-    _localStorage: window ? window.localStorage : { setItem: () => {}, getItem: () => {} },
-    isClient: () => true,
-    isServer: () => false,
-    absoluteUrl: () => 'http://vulcanjs.org/'
-}
+  settings: MeteorSettings,
+  userId: () => 'mock_user_id',
+  startup: () => {},
+  _localStorage: window ? window.localStorage : { setItem: () => {}, getItem: () => {} },
+  isClient: () => true,
+  isServer: () => false,
+  absoluteUrl: () => 'http://vulcanjs.org/',
+};
